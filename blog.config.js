ef1e5351bc195001a8298b65ce9c7537ec803f9f
JOVOVICH <1621499819@qq.com>
2025-04-12 16:46:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -80,7 +80,22 @@ const BLOG = {
   process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
     'https://cdn.jsdelivr.net/npm/live2d-widget-model-koharu/assets/koharu.model.json', 
   // 是否开启点击宠物切换主题
-  WIDGET_PET_SWITCH_THEME: false
+  WIDGET_PET_SWITCH_THEME: false,
+
+  // PrismJs 代码相关
+  PRISM_JS_PATH: 'https://npm.elemecdn.com/prismjs@1.29.0/components/',
+  PRISM_JS_AUTO_LOADER: 'https://npm.elemecdn.com/prismjs@1.29.0/plugins/autoloader/prism-autoloader.min.js',
+
+  // 代码主题 @see https://github.com/PrismJS/prism-themes
+  PRISM_THEME_PREFIX_PATH: 'https://npm.elemecdn.com/prism-themes/themes/prism-one-light.min.css', // 代码块默认主题
+  PRISM_THEME_SWITCH: process.env.NEXT_PUBLIC_PRISM_THEME_SWITCH || true, // 是否开启浅色/深色模式代码主题切换； 开启后将显示以下两个主题
+  PRISM_THEME_LIGHT_PATH: 'https://npm.elemecdn.com/prism-themes/themes/prism-one-light.min.css', // 浅色模式主题
+  PRISM_THEME_DARK_PATH: 'https://npm.elemecdn.com/prism-themes/themes/prism-dracula.min.css', // 深色模式主题
+
+  CODE_MAC_BAR: process.env.NEXT_PUBLIC_CODE_MAC_BAR || true, // 代码左上角显示mac的红黄绿图标
+  CODE_LINE_NUMBERS: process.env.NEXT_PUBLIC_CODE_LINE_NUMBERS || true, // 是否显示行号
+  CODE_COLLAPSE: process.env.NEXT_PUBLIC_CODE_COLLAPSE || true, // 是否折叠代码框
+
 }
 
 module.exports = BLOG
